a880115eafe48a367f54dc65aad543eda9f78061
sakun9526 <sakunrathnayaka@yahoo.com>
2021-02-20 13:15:51 +0530

    htaccess file added to config routes

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'utility', 'language', 'form');
+$autoload['helper'] = array('url', 'file', 'language', 'form');
 
 /*
 | -------------------------------------------------------------------
